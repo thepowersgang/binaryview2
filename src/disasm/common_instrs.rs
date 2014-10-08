9a447d0b3b6965060a4d4164765d3fbcbffa7d02
John Hodge <tpg@mutabah.net>
2014-10-08 19:15:13 +0800

    Common Instructions - Convert all to def_instr!

--- src/disasm/common_instrs.rs
+++ src/disasm/common_instrs.rs
@@ -1,6 +1,8 @@
+// BinaryView2
+// - By John Hodge (thePowetsGang)
 //
-//
-//
+// disasm/common_instrs.rs
+// - Common generic instructions
 #![macro_escape]
 use disasm::InstructionClass;
 use disasm::{InstrParam,ParamTmpReg};
@@ -32,154 +34,73 @@ macro_rules! def_instr{
 	}
 }
 
-struct IClassCall;
-struct IClassMove;
-struct IClassShl;
-struct IClassAdd;
-struct IClassLoadOfs;
-struct IClassStoreOfs;
-
-pub static CALL: IClassCall = IClassCall;
-pub static MOVE: IClassMove = IClassMove;
-pub static SHL: IClassShl = IClassShl;
-pub static ADD: IClassAdd = IClassAdd;
-pub static LOAD_OFS: IClassLoadOfs = IClassLoadOfs;
-pub static STORE_OFS: IClassStoreOfs = IClassStoreOfs;
-
+// JUMP - Shift program execution elsewhere
 def_instr!(JUMP, IClassJump, (f,p,state) => {
 	{ true };
 	{ write!(f, "{}", p[0]) };
 	{ microcode::JUMP.forwards(state, InstrSizeNA, p.slice(0,1)); } ;
-	{ } ;
+	{ fail!("Can't reverse a JUMP"); };
 })
 
-/*
-struct IClassJump;
-pub static JUMP: IClassJump = IClassJump;
-impl InstructionClass for IClassJump
-{
-	fn name(&self) -> &str { "JUMP" }
-	fn is_terminal(&self, _: &[InstrParam]) -> bool { true }
-	fn print(&self, f: &mut ::std::fmt::Formatter, p: &[InstrParam]) -> Result<(),::std::fmt::FormatError>
-	{
-		write!(f, "{}", p[0])
-	}
-	fn forwards(&self, state: &mut State, params: &[InstrParam])
-	{
-		microcode::JUMP.forwards(state, InstrSizeNA, params.slice(0,1));
-	}
-	fn backwards(&self, state: &mut State, params: &[InstrParam])
-	{
-	}
-}
-*/
-
-impl InstructionClass for IClassCall
-{
-	fn name(&self) -> &str {
-		"CALL"
-	}
-	fn is_terminal(&self, _: &[InstrParam]) -> bool {
-		false
-	}
-	fn print(&self, f: &mut ::std::fmt::Formatter, p: &[InstrParam]) -> Result<(),::std::fmt::FormatError>
-	{
-		write!(f, "{}", p[0])
-	}
-	fn forwards(&self, state: &mut State, params: &[InstrParam])
+// CALL - Subroutine call
+// TODO: Needs to handle state munging from subroutine clobbers
+def_instr!(CALL, IClassCall, (f,p,state) => {
+	{ false };
+	{ write!(f, "{}", p[0]) };
 	{
 		//microcode::CALL.forwards(state, params[0..1]);
-	}
-	fn backwards(&self, state: &mut State, params: &[InstrParam])
+	};
 	{
-	}
-}
+		fail!("TODO: CALL.backwards");
+	};
+})
 
-impl InstructionClass for IClassMove
-{
-	fn name(&self) -> &str {
-		"MOVE"
-	}
-	fn is_terminal(&self, _: &[InstrParam]) -> bool {
-		false
-	}
-	fn print(&self, f: &mut ::std::fmt::Formatter, p: &[InstrParam]) -> Result<(),::std::fmt::FormatError>
-	{
-		write!(f, "{}, {}", p[0], p[1])
-	}
-	fn forwards(&self, state: &mut State, params: &[InstrParam])
+// MOVE - Shift a value between registers
+def_instr!(MOVE, IClassMove, (f,params,state) => {
+	{ false };
+	{ write!(f, "{}, {}", params[0], params[1]) };
 	{
 		let val = state.get(params[1]);
 		state.set(params[0], val);
-	}
-	fn backwards(&self, state: &mut State, params: &[InstrParam])
+	};
 	{
 		let val = state.get(params[0]);
 		state.set(params[0], Value::unknown());
 		state.set(params[1], val);
-	}
-}
+	};
+})
 
-impl InstructionClass for IClassShl
-{
-	fn name(&self) -> &str { "SHL" }
-	fn is_terminal(&self, _: &[InstrParam]) -> bool { false }
-	fn print(&self, f: &mut ::std::fmt::Formatter, p: &[InstrParam]) -> Result<(),::std::fmt::FormatError>
-	{
-		unimplemented!();
-	}
-	fn forwards(&self, state: &mut State, params: &[InstrParam]) {
-		unimplemented!();
-	}
-	fn backwards(&self, state: &mut State, params: &[InstrParam]) {
-		unimplemented!();
-	}
-}
+// SHL - Bitwise Shift Left
+def_instr!(SHL, IClassShl, (f, params, state) => {
+	{ false };
+	{ write!(f, "{} := {} << {}", params[0], params[1], params[2]) };
+	{ unimplemented!(); };
+	{ unimplemented!(); };
+})
 
-impl InstructionClass for IClassAdd
-{
-	fn name(&self) -> &str {
-		"ADD"
-	}
-	fn is_terminal(&self, _: &[InstrParam]) -> bool {
-		false
-	}
-	fn print(&self, f: &mut ::std::fmt::Formatter, p: &[InstrParam]) -> Result<(),::std::fmt::FormatError>
-	{
-		write!(f, "{}, {}, {}", p[0], p[1], p[2])
-	}
-	fn forwards(&self, state: &mut State, params: &[InstrParam])
+// ADD - Addition of two values into a register
+def_instr!(ADD, IClassAdd, (f,params,state) => {
+	{ false };
+	{ write!(f, "{}, {}, {}", params[0], params[1], params[2]) };
 	{
 		let val = state.get(params[1]) + state.get(params[2]);
 		state.set(params[0], val);
 		// TODO: Set flags based on val
-	}
-	fn backwards(&self, state: &mut State, params: &[InstrParam])
+	};
 	{
 		unimplemented!();
-	}
-}
+	};
+})
 
-impl InstructionClass for IClassLoadOfs
-{
-	fn name(&self) -> &str {
-		"LOAD"
-	}
-	fn is_terminal(&self, _: &[InstrParam]) -> bool
-	{
-		false
-	}
-	fn print(&self, f: &mut ::std::fmt::Formatter, p: &[InstrParam]) -> Result<(),::std::fmt::FormatError>
-	{
-		write!(f, "{}, [{}+{}]", p[0], p[1], p[2])
-	}
-	fn forwards(&self, state: &mut State, params: &[InstrParam])
+// LOAD (OFS) - Load from a register+offset
+def_instr!(LOAD_OFS, IClassLoadOfs, (f,params,state) => {
+	{ false };
+	{ write!(f, "{}, [{}+{}]", params[0], params[1], params[2]) };
 	{
 		let addr = state.get(params[1]) + state.get(params[2]);
 		state.set( ParamTmpReg(0), addr );
 		microcode::LOAD.forwards(state, ::disasm::InstrSize32, [params[0], ParamTmpReg(0)]);
-	}
-	fn backwards(&self, state: &mut State, params: &[InstrParam])
+	};
 	{
 		if params[0] != params[1] && params[0] != params[2]
 		{
@@ -187,24 +108,18 @@ impl InstructionClass for IClassLoadOfs
 			state.set( ParamTmpReg(0), addr );
 		}
 		microcode::LOAD.backwards(state, ::disasm::InstrSize32, [params[0], ParamTmpReg(0)]);
-	}
-}
+	};
+})
 
-impl InstructionClass for IClassStoreOfs
-{
-	fn name(&self) -> &str { "STORE" }
-	fn is_terminal(&self, _: &[InstrParam]) -> bool { false }
-	fn print(&self, f: &mut ::std::fmt::Formatter, p: &[InstrParam]) -> Result<(),::std::fmt::FormatError>
-	{
-		write!(f, "[{}+{}], {}", p[1], p[2], p[0])
-	}
-	fn forwards(&self, state: &mut State, params: &[InstrParam])
+// STORE (OFS) - Store using an offset from a register
+def_instr!(STORE_OFS, IClassStoreOfs, (f,params,state) => {
+	{ false };
+	{ write!(f, "[{}+{}], {}", params[1], params[2], params[0]) };
 	{
 		let addr = state.get(params[1]) + state.get(params[2]);
 		state.set( ParamTmpReg(0), addr );
 		microcode::STORE.forwards(state, ::disasm::InstrSize32, [params[0], ParamTmpReg(0)]);
-	}
-	fn backwards(&self, state: &mut State, params: &[InstrParam])
+	};
 	{
 		let addr = if params[0] != params[1] && params[0] != params[2] {
 				state.get(params[1]) + state.get(params[2])
@@ -214,7 +129,7 @@ impl InstructionClass for IClassStoreOfs
 			};
 		state.set(ParamTmpReg(0), addr);
 		microcode::STORE.backwards(state, ::disasm::InstrSize32, [params[0], ParamTmpReg(0)]);
-	}
-}
+	};
+})
 
 // vim: ft=rust
